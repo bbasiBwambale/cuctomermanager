8f41cd87179eaf856ab7722a3c5f6202ad05f154
Basi Yonah Bwambale <basiyonah@gmail.com>
2023-07-13 09:21:39 -0400

    added equals, hashcode and toString methods to Customer.java

--- src/main/java/com/ford/customermanager/domain/Customer.java
+++ src/main/java/com/ford/customermanager/domain/Customer.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -21,4 +22,24 @@ public class Customer {
     private UUID customerId;
     @Column(name = "customer_name")
     private String customerName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+        return Objects.equals(getCustomerId(), customer.getCustomerId()) && Objects.equals(getCustomerName(), customer.getCustomerName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCustomerId(), getCustomerName());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                '}';
+    }
 }
